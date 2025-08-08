2d80cb1f683f3ee2e9ff7322a521f8dd6c36c89d
EZARATE4607 <91735375+EZARATE4607@users.noreply.github.com>
2025-08-08 19:29:59 -0400

    d

--- src/ai/dev.ts
+++
@@ -1,7 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/suggest-lunch-menu.ts';
-import '@/ai/flows/create-lunch-event.ts';
-import '@/ai/flows/send-rating-request.ts';
-import '@/ai/flows/migrate-db-flow.ts';
--- src/models/notification.ts
+++
@@ -1,9 +0,0 @@
-
-
-export interface Notification {
-    id: string;
-    title: string;
-    description: string;
-    time: string;
-    read: boolean;
-}
